c0f6f67c4b4474388d935b2f2454d29cef054295
MedBeji <medbeji@MacBook-Pro-de-MedBeji.local>
2017-11-03 18:14:13 +0100

    Initial commit

---
+++ NewsTest/NewVC.swift
@@ -0,0 +1,35 @@
+//
+//  NewVC.swift
+//  NewsTest
+//
+//  Created by MedBeji on 03/11/2017.
+//  Copyright © 2017 TeamIO. All rights reserved.
+//
+
+import UIKit
+
+class NewVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
